134391dfef3d38ca1d26c3fd374a315b0bb61c4e
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-06-23 19:14:46 +0200

    singleton basic tutorial

--- java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/Main.java
+++ java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/Main.java
@@ -1,32 +1,11 @@
 package com.vikman.enterprise.javaooptutorial;
 
+import com.vikman.enterprise.javaooptutorial.exercises.oop.Database;
+
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Running on worker thread");
-                for(int i=0; i<10; i++){
-                    System.out.println(i+ " loop in a worker thread");
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        });
-        thread.start();
-
-        for(int i=0; i<10; i++){
-            System.out.println(i+ " loop in a main thread");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-
+        Database mongoDb = Database.getInstance("MongoDB");
+        System.out.println(mongoDb.getName());
+        System.out.println(mongoDb.toString());
     }
 }
---
+++ java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/exercises/oop/Database.java
@@ -0,0 +1,26 @@
+package com.vikman.enterprise.javaooptutorial.exercises.oop;
+
+public class Database {
+    private String name;
+
+    private static Database instance;
+
+    public static synchronized Database getInstance(String name) {
+        if(instance == null){
+            instance = new Database(name);
+        }
+        return instance;
+    }
+
+    private Database(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
