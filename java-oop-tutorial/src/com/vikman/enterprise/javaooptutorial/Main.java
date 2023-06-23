fed802342151304c9ac96052bb05337cadcc7bf0
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-06-23 17:30:07 +0200

    abstract classes and interfaces tutorial

--- java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/Main.java
+++ java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/Main.java
@@ -1,10 +1,21 @@
 package com.vikman.enterprise.javaooptutorial;
 
-import com.vikman.enterprise.javaooptutorial.exercises.oop.*;
+import com.vikman.enterprise.javaooptutorial.exercises.CarInterface;
+import com.vikman.enterprise.javaooptutorial.exercises.ElectricCar;
+import com.vikman.enterprise.javaooptutorial.exercises.FossilFuelCar;
+import com.vikman.enterprise.javaooptutorial.exercises.School;
+import com.vikman.enterprise.javaooptutorial.exercises.Student;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        DoctorProgram doctorProgram = new DoctorProgram();
-        doctorProgram.runGame();
+        CarInterface teslaCar = new ElectricCar("Tesla");
+        teslaCar.start();
+        teslaCar.move(60);
+        CarInterface toyotaCar = new FossilFuelCar("Toyota");
+        toyotaCar.start();
+        toyotaCar.move(90);
+
+
     }
 }
