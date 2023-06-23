0a75dfa0df786fc6d0d1cd0f8cbc08e185289db1
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-06-23 18:50:43 +0200

    threads basic tutorial

--- java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/Main.java
+++ java-oop-tutorial/src/com/vikman/enterprise/javaooptutorial/Main.java
@@ -1,23 +1,32 @@
 package com.vikman.enterprise.javaooptutorial;
 
-import com.vikman.enterprise.javaooptutorial.exercises.CarInterface;
-import com.vikman.enterprise.javaooptutorial.exercises.ElectricCar;
-import com.vikman.enterprise.javaooptutorial.exercises.FossilFuelCar;
-import com.vikman.enterprise.javaooptutorial.exercises.School;
-import com.vikman.enterprise.javaooptutorial.exercises.Student;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ElectricCar teslaCar = new ElectricCar("Tesla");
-        teslaCar.start();
-        teslaCar.move(60);
-        teslaCar.startRadio();
-        teslaCar.turnRadioOff();
-        FossilFuelCar toyotaCar = new FossilFuelCar("Toyota");
-        toyotaCar.start();
-        toyotaCar.move(90);
-        toyotaCar.startRadio();
-        toyotaCar.turnRadioOff();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running on worker thread");
+                for(int i=0; i<10; i++){
+                    System.out.println(i+ " loop in a worker thread");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        thread.start();
+
+        for(int i=0; i<10; i++){
+            System.out.println(i+ " loop in a main thread");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
     }
 }
